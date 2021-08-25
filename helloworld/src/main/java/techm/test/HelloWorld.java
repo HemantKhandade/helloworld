2c483667e0f25388fee7c444cc5bc00b5ba39ee1
Hemant K <hvlk369@gmail.com>
2021-08-25 23:40:17 +0530

    Update HelloWorld.java

--- helloworld/src/main/java/techm/test/HelloWorld.java
+++ helloworld/src/main/java/techm/test/HelloWorld.java
@@ -3,7 +3,7 @@ package techm.test;
 public class HelloWorld {
 	
 	public String sayHello() { 
-		String hello = "Helloo World";
+		String hello = "Hello World";
 		System.out.println("Say :: " + hello);
 		return hello;
 	}
